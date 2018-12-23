cca23ffa445e9b6f854df6073eb3ad181f1d0dec
And193 <kostinandrey193@gmail.com>
2018-12-23 20:29:05 +0400

    закрытие модального окна по нажатию ESC

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 var popup = document.querySelector(".modal-search");
 var button = document.querySelector(".button--open-order");
 
+popup.classList.remove("modal-search--open");
 popup.classList.add("modal-search--close");
 button.addEventListener("click", function(evt){
   evt.preventDefault();
+  popup.classList.toggle("modal-search--open");
   popup.classList.toggle("modal-search--close");
+});
+document.addEventListener("keydown", function(evt){
+  if(evt.keyCode === 27){
+    popup.classList.remove("modal-search--open");
+    popup.classList.add("modal-search--close");
+  }
 });
